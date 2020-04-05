9b03814c63aa108836d6ee2d22d7d8d98949bccd
Ana Monteiro <44322565+anamonteiro430@users.noreply.github.com>
2020-04-05 19:14:46 +0100

    Add files via upload

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true, // needed for sqlite
+    connection: {
+      filename: './data/file/oldamerica.db3',
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // enforces foreign key constraints on SQLite, not needed for other DBMS
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+  },
+};
